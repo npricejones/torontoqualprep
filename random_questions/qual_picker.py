e1d67790773bc641b8aa1e441ebdb49882d6564f
NatalieP-J <natalie.price.jones@mail.utoronto.ca>
2016-12-04 19:49:52 -0500

    Chose questions

--- random_questions/qual_picker.py
+++ random_questions/qual_picker.py
@@ -9,13 +9,38 @@ Options:
 
 # qual_picker - choose a random qualification exam question not already listed in completed_questions.txt
 
+def check_finished(section,donequestions,maxqs):
+    done = np.array(donequestions)
+    match = done[:,0]==section
+    questions = done[:,1][match]
+    questions.sort()
+    return list(questions) == range(1,maxqs)
+
+def choose_section(section='None'):
+    if section=='None' or isinstance(section,int):
+        section = np.random.randint(1,5+1)
+        if not check_finished(section,donequestions,maxqs[section]):
+            return section
+        elif check_finished(section,donequestions,maxqs[section]):
+            return choose_section(section='None')
+    elif section!='None' and isinstance(section,str):
+        section = int(section)
+        if not check_finished(section,donequestions,maxqs[section]):
+            return section
+        elif check_finished(section,donequestions,maxqs[section]):
+            warn('That section is finished, choosing a new one.')
+            return choose_section(section='None')
+
 import docopt
 import numpy as np
+from warnings import warn
 
 # read in documentation
 arguments = docopt.docopt(__doc__)
 
 section = arguments['--section']
+
+maxqs = {1:20,2:19,3:19,4:22,5:24}
 
 # read in previous questions
 donequestions = np.loadtxt("completed_questions.txt",delimiter=' ',dtype=int)
@@ -26,49 +51,40 @@ if sizecheck > 2:
 elif sizecheck == 2:
     donequestions = [(donequestions[0],donequestions[1])]
 
-
 # Boolean to check if a question not previously done was found
 found=False
 
 while not found:
     # if no section given, randomly select it
-    if section=='None':
-        section = np.random.randint(1,5+1)
-    if section!='None':
-        section = int(section)
+    section = choose_section(section=section)
 
     # find a random question from the appropriate section
     if section == 1:
-        question = np.random.randint(1,20)
-        print section,question
+        question = np.random.randint(1,maxqs[section])
         if (section,question) not in donequestions:
             print 'Do question {0} from the cosmology section'.format(question)
             found = True
 
     if section == 2:
-        question = np.random.randint(1,19)
-        print section,question
+        question = np.random.randint(1,maxqs[section])
         if (section,question) not in donequestions:
             print 'Do question {0} from the extragalactic astronomy section'.format(question)
             found = True
 
     if section == 3:
-        question = np.random.randint(1,19)
-        print section,question
+        question = np.random.randint(1,maxqs[section])
         if (section,question) not in donequestions:
             print 'Do question {0} from the galactic astronomy section'.format(question)
             found = True
 
     if section == 4:
-        question = np.random.randint(1,22)
-        print section,question
+        question = np.random.randint(1,maxqs[section])
         if (section,question) not in donequestions:
             print 'Do question {0} from the stars and planets section'.format(question)
             found = True
 
     if section == 5:
-        question = np.random.randint(1,24)
-        print section,question
+        question = np.random.randint(1,maxqs[section])
         if (section,question) not in donequestions:
             print 'Do question {0} from the math and general physics section'.format(question)
             found = True
